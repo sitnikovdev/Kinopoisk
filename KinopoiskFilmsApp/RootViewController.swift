00ac2bfbfdab11edd680d3981fb9d8fb605704ec
sitnikov <oleg.sitnikov@icloud.com>
2020-02-05 15:11:26 +0300

    Initial Commit

---
+++ KinopoiskFilmsApp/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  KinopoiskFilmsApp
+//
+//  Created by Oleg Sitnikov on 05.02.2020.
+//  Copyright © 2020 Oleg Sitnikov. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

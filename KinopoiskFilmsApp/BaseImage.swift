786a4d61f7957cda5edbbfdcbd5be6ad04244e71
sitnikov <oleg.sitnikov@icloud.com>
2020-02-06 10:01:18 +0300

    Add film description to detail view

---
+++ KinopoiskFilmsApp/BaseImage.swift
@@ -0,0 +1,27 @@
+//
+//  BaseImage.swift
+//  KinopoiskFilmsApp
+//
+//  Created by Oleg Sitnikov on 06.02.2020.
+//  Copyright © 2020 Oleg Sitnikov. All rights reserved.
+//
+
+import UIKit
+
+class BaseImage: UIImageView {
+    
+    
+    init(_ image: UIImage, backgroundColor: UIColor = .purple,   cornerRadius: CGFloat = 0 ) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true
+        self.isOpaque = true 
+        
+        self.image = image
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- KinopoiskFilmsApp/FilmDetailViewController.swift
+++ KinopoiskFilmsApp/FilmDetailViewController.swift
@@ -19,7 +19,11 @@ class FilmDetailViewController: UIViewController {
     let pictureContainer = BaseView(backgroundColor: #colorLiteral(red: 0.1294117647, green: 0.5882352941, blue: 0.9529411765, alpha: 1))
     let descriptionContainer = BaseView(backgroundColor: #colorLiteral(red: 1, green: 0.5960784314, blue: 0, alpha: 1))
     // views
-    let picture = BaseView(backgroundColor: #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1))
+    let pictureImage = BaseImage(#imageLiteral(resourceName: "fight_club.jpg"))
+    let originalLabel = BaseView(backgroundColor: #colorLiteral(red: 0.2588235294, green: 0.2588235294, blue: 0.2588235294, alpha: 1))
+    let yearLabel = BaseLabel(backgroundColor: #colorLiteral(red: 0.6117647059, green: 0.1529411765, blue: 0.6901960784, alpha: 1), text: "1994", textColor: .black)
+    let rating = BaseLabel(backgroundColor: #colorLiteral(red: 0.2980392157, green: 0.6862745098, blue: 0.3137254902, alpha: 1), text: "8.233", textColor: .white)
+    let descriptionLabel = BaseLabel(backgroundColor: #colorLiteral(red: 0.2588235294, green: 0.2588235294, blue: 0.2588235294, alpha: 1), text: "Терзаемый хронической бессонницей и отчаянно пытающийся вырваться из мучительно скучной жизни, клерк встречает некоего Тайлера Дардена, харизматического торговца мылом с извращенной философией. Тайлер уверен, что самосовершенствование — удел слабых, а саморазрушение — единственное, ради чего стоит жить. Пройдет немного времени, и вот уже главные герои лупят друг друга почем зря на стоянке перед баром, и очищающий мордобой доставляет им высшее блаженство. Приобщая других мужчин к простым радостям физической жестокости, они основывают тайный Бойцовский Клуб, который имеет огромный успех. Но в концовке фильма всех ждет шокирующее открытие, которое может привести к непредсказуемым событиям…", textColor: .white)
     
 // MARK: - ViewController Life Cycle
     
@@ -42,7 +46,12 @@ class FilmDetailViewController: UIViewController {
     fileprivate func addViews() {
         // viewContainer
         view.addSubview(viewContainer)
-        pictureContainer.addSubview(picture)
+        pictureContainer.addSubview(pictureImage)
+        descriptionContainer.addSubview(originalLabel)
+        descriptionContainer.addSubview(yearLabel)
+        descriptionContainer.addSubview(rating)
+        
+        bottomContainer.addSubview(descriptionLabel)
 
     }
     
@@ -53,14 +62,34 @@ class FilmDetailViewController: UIViewController {
         bottomContainer.height(500)
         
         pictureContainer.width(200)
+//        descriptionContainer.width(200)
         
         topContainer.stack([pictureContainer, descriptionContainer, UIView()], axis: .horizontal, width: nil, height: nil, spacing: 0)
 
         viewContainer.stack([topContainer,bottomContainer, UIView()], axis: .vertical,  width: nil, height: nil, spacing: 10)
-//
  
-        picture.edges(to: pictureContainer,  insets: TinyEdgeInsets(top: 16, left: 16, bottom: 16, right: 16),  isActive: true)
+        pictureImage.edges(to: pictureContainer,  insets: TinyEdgeInsets(top: 16, left: 16, bottom: 16, right: 16),  isActive: true)
         
+        
+        // original
+        originalLabel.height(25)
+        originalLabel.width(120)
+        originalLabel.top(to: pictureImage, offset: 0)
+
+        
+        // year
+        yearLabel.height(25)
+        yearLabel.width(100)
+        yearLabel.top(to: originalLabel, offset: 32)
+        
+        // rating
+        rating.height(25)
+        rating.width(120)
+        rating.top(to: yearLabel, offset: 32)
+        
+        // description
+         descriptionLabel.edges(to: bottomContainer, excluding: .bottom, insets: TinyEdgeInsets(top: 16, left: 16, bottom: 0, right: 16), isActive: true)
+
     }
     
     
